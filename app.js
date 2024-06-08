f3d555c478f8cd538ea9ac161c85cbadc874fa0a
Shivam Yadav <codecshivam@gmail.com>
2024-06-08 18:03:32 +0530

    setup: env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import { configDotenv } from "dotenv";
+configDotenv();
 import express from "express";
 import authRouter from "./routes/authRoute.js";
-
+const PORT = process.env.APP_PORT || 3001;
 const app = express();
 
 app.get("/", (req, res) => {
@@ -13,6 +15,13 @@ app.get("/", (req, res) => {
 // All routes will be here
 app.use("/api/v1/auth", authRouter);
 
-app.listen(3000, () => {
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    status: "error",
+    message: "resource not found",
+  });
+});
+
+app.listen(PORT, () => {
   console.log("server listening on 3000");
 });
